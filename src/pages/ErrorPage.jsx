f1272f75c0186d0080f3fc599ff1b554a34a4695
unknown <isaruhullah035@gmail.com>
2025-10-09 18:46:44 +0600

    Add some css

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -9,7 +9,7 @@ const ErrorPage = () => {
             <div className='flex flex-col bg-gray-100 justify-center items-center'>
                 <img src={ErrorIcon} alt="OPPS!! APP NOT FOUND" className='w-74 h-74'/>
             <h1 className='font-bold text-3xl text-black mt-5'>Oops, page not found!</h1>
-            <p className='font-normal mt-2'>The page you are looking for is not available.</p>
+            <p className='font-normal text-black mt-2'>The page you are looking for is not available.</p>
             <div>
                 <button onClick={()=> navigate("/")} className='border rounded bg-purple-700 text-white font-bold py-1 px-2 mt-3 mb-4'>Go Back</button>
             </div>
